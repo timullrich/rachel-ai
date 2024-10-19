6a370fe4a1b12f046dfc79c0916bf1ca60d26508
Tim Ullrich <mail@timullrich.de>
2024-10-19 23:30:45 +0200

    Removed unnecessary code.

--- src/executors/command_executor.py
+++ src/executors/command_executor.py
@@ -35,19 +35,6 @@ class CommandExecutor(ExecutorInterface):
         return "No command provided."
 
     def get_result_interpreter_instructions(self, user_language="en") -> str:
-        # return "Please analyze the result of the previous executed command to the user. Ensure the response is: " \
-        #        "Brief and relevant: Avoid unnecessary details. Provide only key insights and point " \
-        #        "out anything unusual or noteworthy. If relevant, summarize the outcome instead " \
-        #        "of listing all details. " \
-        #         "" \
-        #        "Ask follow-up questions: If the result could lead to further action, offer clear," \
-        #        " concise suggestions or ask if the user needs more details or further actions. " \
-        #        "" \
-        #        "Error handling: If an error occurs, explain it simply, suggest possible causes, " \
-        #        "and recommend logical next steps for resolution. Prioritize solutions. " \
-        #        "" \
-        #        f"Please always answer in Language '{user_language}'"
-
         return f"Please explain the output of the previous executed command to the user as short" \
                " as possible and point out the relevant data. Also ask for further instructions! " \
                f"Please always answer in Language '{user_language}'"
